2ee2e37cefb5d17fc07393c5553c88d509296104
udit01 <uditjainiitd@gmail.com>
2025-01-14 13:17:11 +0530

    Added loop

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,17 @@
 int main() {
 
   const std::string error_message = "command not found";
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  while(true) {
+      // Flush after every std::cout / std:cerr
+      std::cout << std::unitbuf;
+      std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+      // Uncomment this block to pass the first stage
+      std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
+      std::string input;
+      std::getline(std::cin, input);
 
-  std::cout << input << ": " << error_message << std::endl;
+      std::cout << input << ": " << error_message << std::endl;
+  }
 }
