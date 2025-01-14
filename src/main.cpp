5f634239c832027210a096bbc1c0b320d806669e
udit01 <uditjainiitd@gmail.com>
2025-01-14 15:00:41 +0530

    Checked for type commands in path

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,96 @@
 #include <iostream>
-#include <cstring>
+#include <string>
+#include <sstream>
+#include <filesystem>
+
+using namespace std;
+
+enum validCommands
+{
+    echo,
+    cd,
+    exit0,
+    type,
+    invalid,
+};
+
+validCommands isValid(std::string command){
+    command = command.substr(0,command.find(" "));
+
+    if(command == "echo") return validCommands::echo;
+    if(command == "cd") return validCommands::cd;
+    if(command == "exit") return validCommands::exit0;
+    if(command == "type") return validCommands::type;
+
+    return invalid;
+}
+
+std::string valid[4] = {"echo", "cd", "exit0"};
+
+std::string get_path(std::string command){
+    std::string path_env = std::getenv("PATH");
+
+    std::stringstream ss(path_env);
+    std::string path;
+
+    while(!ss.eof()){
+        getline(ss, path, ':');
+
+        string abs_path = path + '/' + command;
+
+        if(filesystem::exists(abs_path)){
+            return abs_path;
+        }
+    }
+    return "";  
+}
 
 int main() {
 
-    const std::string error_message = "command not found";
-    const std::string exit_prefix = "exit";
-    const std::string echo_prefix = "echo";
-    const std::string type_prefix = "type";
-    const std::string builtin_command_message = "is a shell builtin";
-    const std::string command_notfound_message = "not found";
-    
-    bool valid_command = false;
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // std::cout << "Logs from your program will appear here!\n";
 
-    while(true) {
+    bool exit = false;
+
+    while (!exit){
         // Flush after every std::cout / std:cerr
         std::cout << std::unitbuf;
         std::cerr << std::unitbuf;
-
-        // Uncomment this block to pass the first stage
         std::cout << "$ ";
-
         std::string input;
         std::getline(std::cin, input);
 
-		int is_prefix_exit = strncmp(exit_prefix.c_str(), input.c_str(), strlen(exit_prefix.c_str()));
-        if(is_prefix_exit == 0){
-            valid_command = true;
-            // If the start of the command, matches the exit, now find the argument
-            std::string exit_arg = input.substr(strlen(exit_prefix.c_str()), std::string::npos);
-            //std::cout << exit_arg <<std::endl; 
-            break;
+        switch(isValid(input)){
+            case cd:
+
+                break;
+            case echo:
+                input.erase(0,input.find(" ")+1);
+                std::cout<<input<<"\n";
+                break;
+            case exit0:
+                exit=true;
+                break;
+            case type:
+                input.erase(0,input.find(" ")+1);
+                if(isValid(input) != invalid){
+                    std::cout<<input<<" is a shell builtin\n";
+                }
+                else{
+                    std::string path = get_path(input);
+
+                    if(path.empty()){
+                        std::cout<<input<<" not found\n";
+                    }
+                    else{
+                        std::cout<<input<<" is "<<path<<std::endl;
+                    }
+                }
+                break;
+            default:
+                std::cout<<input<<": command not found\n";
+                break;
         }
-
-        int is_prefix_echo = strncmp(echo_prefix.c_str(), input.c_str(), strlen(echo_prefix.c_str()));
-        if(is_prefix_echo == 0){
-            valid_command = true;
-            // If the start of the command, matches the echo, now find the argument
-            std::string echo_arg = input.substr(strlen(echo_prefix.c_str()), std::string::npos);
-            // It will have a white space in front
-            if(echo_arg.length()>=1){
-                echo_arg = echo_arg.substr(1, std::string::npos);
-            }
-            std::cout << echo_arg <<std::endl; 
-        }
-
-        int is_prefix_type = strncmp(type_prefix.c_str(), input.c_str(), strlen(type_prefix.c_str()));
-        if(is_prefix_type == 0){
-            valid_command = true;
-            // If the start of the command, matches the type, now find the argument
-            std::string type_arg = input.substr(strlen(type_prefix.c_str()), std::string::npos);
-            // It will have a white space in front
-            if(type_arg.length()>=1){
-                type_arg = type_arg.substr(1, std::string::npos);
-            }
-            std::string output_message = type_arg;
-
-            if((type_arg == exit_prefix) || (type_arg == echo_prefix) || (type_arg == type_prefix)){
-                output_message += " " + builtin_command_message;
-            }else{
-                output_message += ": " + command_notfound_message;
-            }
-            std::cout << output_message <<std::endl; 
-        }
-
-
-        if(valid_command == false){
-            std::cout << input << ": " << error_message << std::endl;
-        }
-
     }
+
 }
