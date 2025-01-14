29d6d127c24e07d0520d088d8a67188332b7867e
udit01 <uditjainiitd@gmail.com>
2025-01-14 14:14:48 +0530

    Checked for type commands validity

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main() {
     const std::string error_message = "command not found";
     const std::string exit_prefix = "exit";
     const std::string echo_prefix = "echo";
+    const std::string type_prefix = "type";
+    const std::string builtin_command_message = "is a shell builtin";
+    const std::string command_notfound_message = "not found";
+    
     bool valid_command = false;
 
     while(true) {
@@ -39,6 +43,26 @@ int main() {
             }
             std::cout << echo_arg <<std::endl; 
         }
+
+        int is_prefix_type = strncmp(type_prefix.c_str(), input.c_str(), strlen(type_prefix.c_str()));
+        if(is_prefix_type == 0){
+            valid_command = true;
+            // If the start of the command, matches the type, now find the argument
+            std::string type_arg = input.substr(strlen(type_prefix.c_str()), std::string::npos);
+            // It will have a white space in front
+            if(type_arg.length()>=1){
+                type_arg = type_arg.substr(1, std::string::npos);
+            }
+            std::string output_message = type_arg;
+
+            if((type_arg == exit_prefix) || (type_arg == echo_prefix)){
+                output_message += " " + builtin_command_message;
+            }else{
+                output_message += ": " + command_notfound_message;
+            }
+            std::cout << output_message <<std::endl; 
+        }
+
 
         if(valid_command == false){
             std::cout << input << ": " << error_message << std::endl;
