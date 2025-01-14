c745379edcbe281ae48c5c5742122973a716ca20
udit01 <uditjainiitd@gmail.com>
2025-01-14 13:43:08 +0530

    Checked for exit and exited

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <cstring>
 
 int main() {
 
-  const std::string error_message = "command not found";
-  while(true) {
-      // Flush after every std::cout / std:cerr
-      std::cout << std::unitbuf;
-      std::cerr << std::unitbuf;
+    const std::string error_message = "command not found";
+    const std::string exit_prefix = "exit";
 
-      // Uncomment this block to pass the first stage
-      std::cout << "$ ";
+    while(true) {
+        // Flush after every std::cout / std:cerr
+        std::cout << std::unitbuf;
+        std::cerr << std::unitbuf;
 
-      std::string input;
-      std::getline(std::cin, input);
+        // Uncomment this block to pass the first stage
+        std::cout << "$ ";
 
-      std::cout << input << ": " << error_message << std::endl;
-  }
+        std::string input;
+        std::getline(std::cin, input);
+
+		int is_prefix_exit = strncmp(exit_prefix.c_str(), input.c_str(), strlen(exit_prefix.c_str()));
+
+        if(is_prefix_exit == 0){
+            // If the start of the command, matches the exit, now find the argument
+            std::string exit_arg = input.substr(strlen(exit_prefix.c_str()), std::string::npos);
+            //std::cout << exit_arg <<std::endl; 
+            break;
+        }
+
+        std::cout << input << ": " << error_message << std::endl;
+
+    }
 }
